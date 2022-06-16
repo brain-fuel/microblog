7158b45875654543842e7b407e80926d93b9d6cf
Matt Laine <matt@brain-fuel.co>
2022-06-16 15:07:04 -0400

    fix 1 to many relationship in models

--- app/models.py
+++ app/models.py
@@ -6,6 +6,7 @@ class User(db.Model):
     username      = db.Column(db.String(64),  index=True, unique=True)
     email         = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    posts     = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -15,7 +16,6 @@ class Post(db.Model):
     body      = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id   = db.Column(db.Integer, db.ForeignKey('user.id'))
-    posts     = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
